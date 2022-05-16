98de3a209033e5f787a01715f58b6875d8321f59
Michael Munson <michaelmunson151@gmail.com>
2022-05-15 23:30:51 -0500

    update

--- react/prop-it-up/src/components/PersonCard.jsx
+++ react/prop-it-up/src/components/PersonCard.jsx
@@ -2,12 +2,22 @@ import React, { Component } from 'react';
     
     
 class PersonCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: this.props.age
+        }
+    }
     render() {
+        const birthday = () => {
+            this.setState({ age: this.state.age+1 });
+        }
         return (
             <div>
                 <h4>{this.props.lastName}, {this.props.firstName}</h4>
-                <p>Age: {this.props.age}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair Color: {this.props.hairColor}</p>
+                <button onClick={birthday}>Birthday Button for {this.props.firstName} {this.props.lastName}</button>
             </div>
         )
     }
